fa4c4d747a8d2420b98a62c3cdb7b79734059000
vtoanb <vtoanb@gmail.com>
2016-10-20 23:00:02 +0700

    created session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,20 @@ class SessionsController < ApplicationController
   end
 
   def create
+  	user = User.find_by(email: session_params[:email].downcase)
+  	if user && user.authenticate(session_params[:password])
+  		redirect_to root_path, notice: "loged in successfully"
+  	else
+  		render 'new', flash: "Identity not correct, login again!"
+  	end
   end
 
   def destroy
+  end
+
+  private
+
+  def session_params
+  	params.require(:session).permit(:email, :password)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
 
+  get 'signup', to: 'users#new'
+
+  get 'login', to: 'sessions#new'
+
+  post 'login', to: 'sessions#create'
+
+  get 'signout', to: 'sessions#destroy'
+
   get 'sessions/new'
 
   get 'sessions/create'
