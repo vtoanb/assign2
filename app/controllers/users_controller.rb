0a9a5ddb5a4d806ea4325aecee434a5ced15d2aa
vtoanb <vtoanb@gmail.com>
2016-10-20 22:33:05 +0700

    done create new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,30 @@
 class UsersController < ApplicationController
+	
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.create(user_params)
+
+		if @user.save
+			redirect_to root_path, notice: "User created successfully"
+		else
+			render 'new'
+		end
+	end
+
+	def show
+		@user = User.find(:id)
+	end
+
+	def index
+		@user = User.all
+	end
+
+	private
+
+	def user_params
+		params.require(:user).permit(:password, :email, :name)
+	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161017124306) do
+ActiveRecord::Schema.define(version: 20161020152224) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20161017124306) do
     t.string   "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "name"
     t.index ["email"], name: "index_users_on_email", using: :btree
   end
 
