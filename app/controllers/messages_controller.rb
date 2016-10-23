5561bd15f8ea603efd7eae8916f3940f2871a84e
vtoanb <vtoanb@gmail.com>
2016-10-23 09:24:47 +0700

    build nested form

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -17,12 +17,33 @@ class MessagesController < ApplicationController
   end
 
   def new
-    @message = Message.new
+    if user_logged_in?
+      @message = @current_user.inbox.messages.new
+      # 2.times { @message.recipients.build }
+      @message.recipients.build
+    end
   end
 
   def create
+    if user_logged_in?
+      @message = @current_user.inbox.messages.new(message_params)
+
+      if @message.save
+        flash[:notice] =  "message sent successfully!"
+        redirect_to @current_user
+      else
+        flash[:notice] = "#{errors.full_messages.to_sentence}"
+        render 'new'
+      end
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:subject, :content, recipients_attributes: [:id, :user_id])
   end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,6 @@ class Message < ApplicationRecord
   belongs_to :inbox
   has_many :recipients
   has_many :users, through: :recipients
+  # intend to build nested form, first need to config this model
+  accepts_nested_attributes_for :recipients
 end
